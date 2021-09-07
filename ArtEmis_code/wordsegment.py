23a6a9baa6dce54c4343a22c264141316a44dfe2
Ananya Devarakonda <57040724+ananya314@users.noreply.github.com>
2021-09-07 21:27:31 +0530

    Updated wordsegment.py
    added descriptions of the datasets used to train the VAE model

--- ArtEmis_code/wordsegment.py
+++ ArtEmis_code/wordsegment.py
@@ -3,8 +3,15 @@ import pandas as pd
 from wordsegment import load, segment
 
 path = "/content/drive/MyDrive/Lumiere/Dataset/artemis_data.csv"
+
+# artemis_data.csv is a csv file that contains all captions from the ArtEmis dataset. It has two columns TEXT and LABEL that both contain ArtEmis captions. 
+# TEXT and LABEL are the same because a VAE learns to reconstruct data. Hence, the expected output (label) is same as the input.
+
 artemis_data = pd.read_csv(path)
 load()
+
+# Few captions in the ArtEmis dataset do not have spaces between words and look like: anexamplesentence. 
+# wordsegment helps to add spaces in this circumstance.
 
 while i < artemis_data.shape[0]:
   if i % 100 == 0:
@@ -14,6 +21,7 @@ while i < artemis_data.shape[0]:
 
   i = i + 1
 
+# saving the preprocessed dataset as actual_sentences.csv
 import os
 path = "/content/drive/MyDrive/Lumiere/"
 os.chdir(path)
